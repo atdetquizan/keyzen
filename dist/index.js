5b590a3278691a9bc5fde6681806c330a0d9573d
anthony fernando torres detquizan <anthonyfernandotorresdetquizan@MacBook-Pro-de-anthony.local>
2025-04-13 10:57:44 -0500

    feat: first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.keyzen = keyzen;
+function keyzen() {
+    console.log("KEYZEN");
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function keyzen() {
+  console.log("KEYZEN");
+}
